656f6c9c913cbc27483c778525cd62bd95711fe5
Matheus Germano de Moraes <matheusgermano97@hotmail.com>
2020-01-03 17:05:19 -0300

    r

--- js/menu.js
+++ js/menu.js
@@ -37,21 +37,84 @@ $(".depto-eletrico").hover(()=>{
     $(".depto-eletrico#eletrico").hide()
 })
 
-$(".depto-material").hover(()=>{
+$(".depto-hidraulico").hover(()=>{
     
-    $(".depto-material#material").show()
+    $(".depto-hidraulico#hidraulico").show()
 }, 
 ()=>{
-    $(".depto-material#material").hide()
+    $(".depto-hidraulico#hidraulico").hide()
 })
-$(".depto-prevencao").hover(()=>{
+$(".depto-incendio").hover(()=>{
     
-    $(".depto-prevencao#prevencao").show()
+    $(".depto-incendio#incendio").show()
 }, 
 ()=>{
-    $(".depto-prevencao#prevencao").hide()
+    $(".depto-incendio#incendio").hide()
 })
 
+$(".depto-casa").hover(()=>{
+    
+    $(".depto-casa#casa").show()
+}, 
+()=>{
+    $(".depto-casa#casa").hide()
+})
+
+$(".depto-chuveiro").hover(()=>{
+    
+    $(".depto-chuveiro#chuveiro").show()
+}, 
+()=>{
+    $(".depto-chuveiro#chuveiro").hide()
+})
+
+$(".depto-ferramentas").hover(()=>{
+    
+    $(".depto-ferramentas#ferramentas").show()
+}, 
+()=>{
+    $(".depto-ferramentas#ferramentas").hide()
+})
+
+$(".depto-loucas").hover(()=>{
+    
+    $(".depto-loucas#loucas").show()
+}, 
+()=>{
+    $(".depto-loucas#loucas").hide()
+})
+
+$(".depto-medidores").hover(()=>{
+    
+    $(".depto-loucas#medidores").show()
+}, 
+()=>{
+    $(".depto-loucas#medidores").hide()
+})
+
+$(".depto-sinalizacao").hover(()=>{
+    
+    $(".depto-sinalizacao#sinalizacao").show()
+}, 
+()=>{
+    $(".depto-sinalizacao#sinalizacao").hide()
+})
+
+$(".depto-tinta").hover(()=>{
+    
+    $(".depto-tinta#tinta").show()
+}, 
+()=>{
+    $(".depto-tinta#tinta").hide()
+})
+
+$(".depto-ventilador").hover(()=>{
+    
+    $(".depto-ventilador#ventilador").show()
+}, 
+()=>{
+    $(".depto-ventilador#ventilador").hide()
+})
 
 
 
@@ -64,93 +127,203 @@ $(".depto-prevencao").hover(()=>{
 
 //  Controle para menu mobile 
 
-
-$(".dropdown-btn#rouparia").toggle(() => {
+$(".dropdown-container").hide();
+$(".dropdown-btn#aquecedores").toggle(() => {
     $(".dropdown-container").hide();
-    $(".dropdown-btn#rouparia i").attr('class', 'fa fa-angle-up');
-    $(".dropdown-container#rouparia").slideDown()
+    $(".dropdown-btn#aquecedores i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#aquecedores").slideDown()
 }, () => {
-    $(".dropdown-container#rouparia").hide()
-    $(".dropdown-btn#rouparia i").attr('class', 'fa fa-angle-down');
+    $(".dropdown-container#aquecedores").slideUp()
+    $(".dropdown-btn#aquecedores i").attr('class', 'fa fa-angle-down');
 })
 
 
-$(".dropdown-btn#tableware").toggle(() => {
+$(".dropdown-container").hide();
+$(".dropdown-btn#aquecedorsolar").toggle(() => {
     $(".dropdown-container").hide();
-    $(".dropdown-btn#tableware i").attr('class', 'fa fa-angle-up');
-    $(".dropdown-container#tableware").slideDown()
+    $(".dropdown-btn#aquecedorsolar i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#aquecedorsolar").slideDown()
 }, () => {
-    $(".dropdown-container#tableware").hide()
-    $(".dropdown-btn#tableware i").attr('class', 'fa fa-angle-down');
+    $(".dropdown-container#aquecedorsolar").slideUp()
+    $(".dropdown-btn#aquecedorsolar i").attr('class', 'fa fa-angle-down');
 })
 
 
-$(".dropdown-btn#decor").toggle(() => {
+$(".dropdown-container").hide();
+$(".dropdown-btn#aquecedorgas").toggle(() => {
     $(".dropdown-container").hide();
-    $(".dropdown-btn#decor i").attr('class', 'fa fa-angle-up');
-    $(".dropdown-container#decor").slideDown()
+    $(".dropdown-btn#aquecedorgas i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#aquecedorgas").slideDown()
 }, () => {
-    $(".dropdown-container#decor").hide()
-    $(".dropdown-btn#decor i").attr('class', 'fa fa-angle-down');
+    $(".dropdown-container#aquecedorgas").slideUp()
+    $(".dropdown-btn#aquecedorgas i").attr('class', 'fa fa-angle-down');
 })
 
 
-$(".dropdown-btn#moveis").toggle(() => {
+$(".dropdown-container").hide();
+$(".dropdown-btn#casa").toggle(() => {
     $(".dropdown-container").hide();
-    $(".dropdown-btn#moveis i").attr('class', 'fa fa-angle-up');
-    $(".dropdown-container#moveis").slideDown()
+    $(".dropdown-btn#casa i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#casa").slideDown()
 }, () => {
-    $(".dropdown-container#moveis").hide()
-    $(".dropdown-btn#moveis i").attr('class', 'fa fa-angle-down');
+    $(".dropdown-container#casa").slideUp()
+    $(".dropdown-btn#casa i").attr('class', 'fa fa-angle-down');
 })
 
 
-$(".dropdown-btn#presentes").toggle(() => {
+
+$(".dropdown-container").hide();
+$(".dropdown-btn#chuveiro").toggle(() => {
     $(".dropdown-container").hide();
-    $(".dropdown-btn#presentes i").attr('class', 'fa fa-angle-up');
-    $(".dropdown-container#presentes").slideDown()
+    $(".dropdown-btn#chuveiro i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#chuveiro").slideDown()
 }, () => {
-    $(".dropdown-container#presentes").hide()
-    $(".dropdown-btn#presentes i").attr('class', 'fa fa-angle-down');
+    $(".dropdown-container#chuveiro").slideUp()
+    $(".dropdown-btn#chuveiro i").attr('class', 'fa fa-angle-down');
 })
 
 
-$(".dropdown-btn#aromas").toggle(() => {
+
+$(".dropdown-container").hide();
+$(".dropdown-btn#ferramentas").toggle(() => {
     $(".dropdown-container").hide();
-    $(".dropdown-btn#aromas i").attr('class', 'fa fa-angle-up');
-    $(".dropdown-container#aromas").slideDown()
+    $(".dropdown-btn#ferramentas i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#ferramentas").slideDown()
 }, () => {
-    $(".dropdown-container#aromas").hide()
-    $(".dropdown-btn#aromas i").attr('class', 'fa fa-angle-down');
+    $(".dropdown-container#ferramentas").slideUp()
+    $(".dropdown-btn#ferramentas i").attr('class', 'fa fa-angle-down');
 })
 
 
-$(".dropdown-btn#baby").toggle(() => {
+
+
+$(".dropdown-container").hide();
+$(".dropdown-btn#iluminacao").toggle(() => {
     $(".dropdown-container").hide();
-    $(".dropdown-btn#baby i").attr('class', 'fa fa-angle-up');
-    $(".dropdown-container#baby").slideDown()
+    $(".dropdown-btn#iluminacao i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#iluminacao").slideDown()
 }, () => {
-    $(".dropdown-container#baby").hide()
-    $(".dropdown-btn#baby i").attr('class', 'fa fa-angle-down');
+    $(".dropdown-container#iluminacao").slideUp()
+    $(".dropdown-btn#iluminacao i").attr('class', 'fa fa-angle-down');
 })
 
 
-$(".dropdown-btn#custom").toggle(() => {
+
+$(".dropdown-container").hide();
+$(".dropdown-btn#loucas").toggle(() => {
     $(".dropdown-container").hide();
-    $(".dropdown-btn#moveis i").attr('class', 'fa fa-angle-up');
-    $(".dropdown-container#custom").slideDown()
+    $(".dropdown-btn#loucas i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#loucas").slideDown()
 }, () => {
-    $(".dropdown-container#custom").hide()
-    $(".dropdown-btn#custom i").attr('class', 'fa fa-angle-down');
+    $(".dropdown-container#loucas").slideUp()
+    $(".dropdown-btn#loucas i").attr('class', 'fa fa-angle-down');
 })
 
 
-$(".dropdown-btn#marcas").toggle(() => {
+$(".dropdown-container").hide();
+$(".dropdown-btn#eletrico").toggle(() => {
     $(".dropdown-container").hide();
-    $(".dropdown-btn#marcas i").attr('class', 'fa fa-angle-up');
-    $(".dropdown-container#marcas").slideDown()
+    $(".dropdown-btn#eletrico i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#eletrico").slideDown()
 }, () => {
-    $(".dropdown-container#marcas").hide()
-    $(".dropdown-btn#marcas i").attr('class', 'fa fa-angle-down');
+    $(".dropdown-container#eletrico").slideUp()
+    $(".dropdown-btn#eletrico i").attr('class', 'fa fa-angle-down');
 })
+
+
+
+$(".dropdown-container").hide();
+$(".dropdown-btn#hidraulico").toggle(() => {
+    $(".dropdown-container").hide();
+    $(".dropdown-btn#hidraulico i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#hidraulico").slideDown()
+}, () => {
+    $(".dropdown-container#hidraulico").slideUp()
+    $(".dropdown-btn#hidraulico i").attr('class', 'fa fa-angle-down');
+})
+
+
+$(".dropdown-container").hide();
+$(".dropdown-btn#medidores").toggle(() => {
+    $(".dropdown-container").hide();
+    $(".dropdown-btn#medidores i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#medidores").slideDown()
+}, () => {
+    $(".dropdown-container#medidores").slideUp()
+    $(".dropdown-btn#medidores i").attr('class', 'fa fa-angle-down');
+})
+
+
+
+$(".dropdown-container").hide();
+$(".dropdown-btn#medidoresagua").toggle(() => {
+    $(".dropdown-container").hide();
+    $(".dropdown-btn#medidoresagua i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#medidoresagua").slideDown()
+}, () => {
+    $(".dropdown-container#medidoresagua").slideUp()
+    $(".dropdown-btn#medidoresagua i").attr('class', 'fa fa-angle-down');
+})
+
+$(".dropdown-container").hide();
+$(".dropdown-btn#medidoresgas").toggle(() => {
+    $(".dropdown-container").hide();
+    $(".dropdown-btn#medidoresgas i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#medidoresgas").slideDown()
+}, () => {
+    $(".dropdown-container#medidoresgas").slideUp()
+    $(".dropdown-btn#medidoresgas i").attr('class', 'fa fa-angle-down');
+})
+
+
+$(".dropdown-container").hide();
+$(".dropdown-btn#sinalizacao").toggle(() => {
+    $(".dropdown-container").hide();
+    $(".dropdown-btn#sinalizacao i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#sinalizacao").slideDown()
+}, () => {
+    $(".dropdown-container#sinalizacao").slideUp()
+    $(".dropdown-btn#sinalizacao i").attr('class', 'fa fa-angle-down');
+})
+
+
+$(".dropdown-container").hide();
+$(".dropdown-btn#incendio").toggle(() => {
+    $(".dropdown-container").hide();
+    $(".dropdown-btn#incendio i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#incendio").slideDown()
+}, () => {
+    $(".dropdown-container#incendio").slideUp()
+    $(".dropdown-btn#incendio i").attr('class', 'fa fa-angle-down');
+})
+
+
+
+$(".dropdown-container").hide();
+$(".dropdown-btn#tintas").toggle(() => {
+    $(".dropdown-container").hide();
+    $(".dropdown-btn#tintas i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#tintas").slideDown()
+}, () => {
+    $(".dropdown-container#tintas").slideUp()
+    $(".dropdown-btn#tintas i").attr('class', 'fa fa-angle-down');
+})
+
+
+$(".dropdown-container").hide();
+$(".dropdown-btn#ventilador").toggle(() => {
+    $(".dropdown-container").hide();
+    $(".dropdown-btn#ventilador i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#ventilador").slideDown()
+}, () => {
+    $(".dropdown-container#ventilador").slideUp()
+    $(".dropdown-btn#ventilador i").attr('class', 'fa fa-angle-down');
+})
+
+
+
+
+
+
+
 
